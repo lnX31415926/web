be35a1646ba01d7b74489dabe2915def0fae13fc
lnx31415926 <xazu819712@163.com>
2021-10-23 20:27:51 +0800

    day03-day05   推荐 和  个人信息  页面的基本实现

---
+++ day03/js/my.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	// $(document).ready() 方法允许我们在文档完全加载完后执行函数。
+$("#tuijian-modle-click").click(function(){
+		if(!($("#3").attr("class")=="item")){
+			// alert(!($("#1").attr("class")=="item"));  1
+			$("#modle").text("影视模块");
+		 }
+		 if(!($("#1").attr("class")=="item")){
+			// ($("#2").attr("class")=="item")&($("#3").attr("class")=="item")    2  后加载
+			$("#modle").text("学习模块");
+		}
+		 if(!($("#2").attr("class")=="item")){
+			// ($("#1").attr("class")=="item")&($("#3").attr("class")=="item")    3
+			$("#modle").text("游戏模块");
+		}
+		
+});
+
+// $("#modle-click").click(function(){
+// 	// 100   010     001
+// 			// active--1
+// 			alert(!($("#1").attr("class")=="item"));  
+	
+// 			// active--2
+// 			alert(!($("#2").attr("class")=="item")); 
+			
+// 			// active--3
+// 			alert(!($("#3").attr("class")=="item")); 
+			
+		
+		
+// });
+});
